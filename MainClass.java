349af7a6fbadd3260f13c5ab7dd716217625d31a
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-10-14 16:01:14 +0800

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,17 @@
+package ImageHandle;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MainClass {
+	public static void main(String []args) throws IOException{
+		//File file=new File("D:\\imageHandle\\test.bmp");
+		//BufferedImage image=ImageIO.read(file);	
+		//BitMap.toGray(image);
+		//BitMap.bitPlane(image);
+		UI.main(null);
+	}
+}
