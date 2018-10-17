846645002f21b069df8c0e4c94d515bb8eff42cd
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-10-17 16:57:19 +0800

    Update BitMap.java
    这是界面布局修改后的稳定版本

--- BitMap.java
+++ BitMap.java
@@ -16,8 +16,8 @@ public class BitMap {
 	
 	public static int hist[]=new int[256];
 	
-	//将彩色位图转换为灰度图
-	public static void toGray(BufferedImage image) throws IOException {
+	//将24位彩色位图转换为灰度图
+	public static BufferedImage toGray(BufferedImage image) throws IOException {
 		File file=new File("D:\\imageHandle\\Gray.bmp");
 		int imgWidth=image.getWidth();
 		int imgHeight=image.getHeight();
@@ -26,21 +26,22 @@ public class BitMap {
 		
 		for(int i=0;i<imgHeight;i++) {
 			for(int j=0;j<imgWidth;j++) {
-				int rgb=image.getRGB(i, j);
+				int rgb=image.getRGB(j, i);//注意这里i和j表达的含义，不能弄反位置
 				int r=cm.getRed(rgb);
 				int g=cm.getGreen(rgb);
 				int b=cm.getBlue(rgb);
 				int h=(int)(0.299 * r + 0.587 * g + 0.114 * b);
-				gray.setRGB(i, j, new Color(h,h,h).getRGB());
+				gray.setRGB(j, i, new Color(h,h,h).getRGB());
 			}
 		}
-		String format="bmp";
+		return gray;
+		/*String format="bmp";
 		try {
 			ImageIO.write(gray, format, file);//将image对象保存至文件
 			System.out.println("保存至灰度图成功.");
 		}catch (IOException e) {
 			System.out.println("保存至灰度图失败.");
-		}
+		}*/
 	}
 	
 	//将8位灰度图分解为8幅位平面
@@ -53,10 +54,10 @@ public class BitMap {
 			img[i] = new BufferedImage( imgWidth,imgHeight, BufferedImage.TYPE_BYTE_GRAY);
 			for(int j=0;j<imgHeight;j++) {
 				for(int k=0;k<imgWidth;k++) {
-					int rgb=gray.getRGB(j, k);
+					int rgb=gray.getRGB(k, j);
 					int red=cModel.getRed(rgb);
 					red=red&(1<<i);
-					img[i].setRGB(j, k,new Color(red,red,red).getRGB());
+					img[i].setRGB(k, j,new Color(red,red,red).getRGB());
 				}
 			}
 			File file=new File("D:\\imageHandle\\bitPlane"+(i+1)+".bmp");
@@ -156,43 +157,71 @@ public class BitMap {
 		return hist;
 	}
 	
-	//画灰度直方图
-	public static BufferedImage drawHist(int []hist,int pixSum) {
-		int size = 300;
-		BufferedImage pic = new BufferedImage(size,size, BufferedImage.TYPE_4BYTE_ABGR);
-		Graphics2D g2d = pic.createGraphics();  
-	    g2d.setPaint(Color.BLACK);  
-	    g2d.fillRect(0, 0, size, size);  
-	    g2d.setPaint(Color.WHITE);  
-        g2d.drawLine(30, 280, 290, 280); //横轴，长度为250
-        g2d.drawLine(290, 280, 285, 277);
-        g2d.drawLine(290, 280, 285, 283);
-        g2d.drawString("灰度级  255", 220, 295);//横轴最右
-        g2d.drawString("0", 27, 280+13);//原点
-	    g2d.drawLine(30, 280, 30, 30);//纵轴，长度为250 	
-	    g2d.drawLine(30, 30, 27, 35);
-	    g2d.drawLine(30, 30, 33, 35);
-	    g2d.drawString("出现频率", 27, 28);//纵轴最高点
-	    //g2d.drawString("出现频率", 27, 15);//纵轴最高点
-	           
-	    g2d.setPaint(Color.GREEN);  
-	    int max = 0;//记录最大灰度级
-	    for ( int i = 0; i<256; i++ )
-        {
-        	if( hist[i] > max ) max = hist[i];
-        }
-          
-        int offset = 1;  
-        for(int i=0; i<hist.length; i++) {  
-        	int lineLength = (int)(250*hist[i]/(double)max);//灰度直方图中的线长度  
-            g2d.drawLine(30 + offset + i, 280, 30 + offset + i, 280-lineLength);  
-        } 
-        //g2d.drawString("最大灰度级:"+String.valueOf(max), 130, 295); 
-           
-      //  g2d.setPaint(Color.RED);  
-       // g2d.drawString("", 100, 270); 
-        return pic;
+	//得到直方图各种统计信息
+	public static void histLabel(int sum,int []hist) {
+		int average=0;
+		int mid=-1;
+		double deviation=0;
+		int n=0;//n用于寻找中位数,特别注意全黑全白的情况下mid的取值
+	
+		for(int i=0;i<hist.length;i++){
+			average+=(double)i*hist[i];
+			if(n<sum/2)
+				n+=hist[i];
+			else {
+				if(mid==-1)
+					mid=i;
+			}
+		}
+		if(hist[0]==sum)//全黑的情况
+			mid=0;
+		if(hist[255]==sum)//全白的情况
+			mid=255;
+		average/=(double)sum;
+		UI.average2.setText("平均灰度："+String.valueOf(average));
+		UI.mid2.setText("中值灰度："+String.valueOf(mid));
+		UI.pixSum2.setText("像素总数："+String.valueOf(sum));
+		
+		for ( int i=0; i<hist.length; i++ )
+		{
+			deviation += (double) (i-average)*(i-average)*hist[i]/(double)sum;
+		}
+		deviation=Math.sqrt(deviation);
+		UI.deviation2.setText("灰度标准差："+String.valueOf(new java.text.DecimalFormat("#.00").format(deviation)));
 	}
+	
+	//画灰度直方图
+		public static BufferedImage drawHist(int []hist,int pixSum) {
+			BufferedImage pic=new BufferedImage(370, 300, BufferedImage.TYPE_4BYTE_ABGR);
+			Graphics2D g2d=pic.createGraphics();
+			g2d.setPaint(Color.BLACK);
+			g2d.fillRect(0, 0, 370, 300);
+			g2d.setPaint(Color.WHITE);  
+	        g2d.drawLine(55, 255, 320, 255); //横轴，长度为200
+	        g2d.drawLine(320, 255, 315, 252);
+	        g2d.drawLine(320,255 , 315, 258);
+	        g2d.drawString("灰度级  255", 250, 268);//横轴最右
+	        g2d.drawString("0", 55-5, 255+12);//原点
+	        g2d.drawLine(55, 255, 55, 55);//纵轴，长度200
+	        g2d.drawLine(55, 55, 52, 60);
+	        g2d.drawLine(55, 55 ,58, 60);
+	        g2d.drawString("出现频率", 55-5, 55-5);//纵轴最高点
+	        
+	        g2d.setPaint(Color.GREEN);  
+		    int max = 0;//记录最大灰度级
+		    for ( int i = 0; i<256; i++ )
+	        {
+	        	if( hist[i] > max ) max = hist[i];
+	        }
+	          
+	        int offset = 1;//从x=1开始画
+	        for(int i=0; i<hist.length; i++) {  
+	        	int lineLength = (int)(200*hist[i]/(double)max);//灰度直方图中的线长度，需要注意的是这里的线长度只反应不同灰度级之间的比例关系，相加并不为全集  
+	            g2d.drawLine(55 + offset + i, 255, 55 + offset + i, 255-lineLength);  
+	        } 
+			return pic;
+		}
+		
 	
 	//线性点处理函数1，整体变亮50个灰度级
 	public static BufferedImage dot_change_1(BufferedImage image) {
@@ -202,11 +231,11 @@ public class BitMap {
 		ColorModel cm=ColorModel.getRGBdefault();
 		for(int i=0;i<ih;i++) {
 			for(int j=0;j<iw;j++) {
-				int rgb=image.getRGB(i, j);
+				int rgb=image.getRGB(j, i);
 				int red=cm.getRed(rgb)+50;
 				if(red>255)
 					red=255;
-				result.setRGB(i, j, new Color(red,red,red).getRGB());
+				result.setRGB(j, i, new Color(red,red,red).getRGB());
 			}
 		}
 		return result;
@@ -220,11 +249,11 @@ public class BitMap {
 		ColorModel cm=ColorModel.getRGBdefault();
 		for(int i=0;i<ih;i++) {
 			for(int j=0;j<iw;j++) {
-				int rgb=image.getRGB(i, j);
+				int rgb=image.getRGB(j, i);
 				int red=cm.getRed(rgb)*2;
 				if(red>255)
 					red=255;
-				result.setRGB(i, j, new Color(red,red,red).getRGB());
+				result.setRGB(j, i, new Color(red,red,red).getRGB());
 			}
 		}
 		return result;
@@ -238,13 +267,13 @@ public class BitMap {
 		ColorModel cm=ColorModel.getRGBdefault();
 		for(int i=0;i<ih;i++) {
 			for(int j=0;j<iw;j++) {
-				int rgb=image.getRGB(i, j);
+				int rgb=image.getRGB(j, i);
 				int red=(int)Math.log(cm.getRed(rgb));
 				if(red>255)
 					red=255;
 				if(red<0)
 					red=0;
-				result.setRGB(i, j, new Color(red,red,red).getRGB());
+				result.setRGB(j, i, new Color(red,red,red).getRGB());
 			}
 		}
 		return result;
@@ -258,13 +287,13 @@ public class BitMap {
 		ColorModel cm=ColorModel.getRGBdefault();
 		for(int i=0;i<ih;i++) {
 			for(int j=0;j<iw;j++) {
-				int rgb=image.getRGB(i, j);
+				int rgb=image.getRGB(j, i);
 				int red=(int)Math.sin(cm.getRed(rgb));
 				if(red>255)
 					red=255;
 				if(red<0)
 					red=0;
-				result.setRGB(i, j, new Color(red,red,red).getRGB());
+				result.setRGB(j, i, new Color(red,red,red).getRGB());
 			}
 		}
 		return result;
@@ -278,15 +307,17 @@ public class BitMap {
 			ColorModel cm=ColorModel.getRGBdefault();
 			for(int i=0;i<ih;i++) {
 				for(int j=0;j<iw;j++) {
-					int rgb=image.getRGB(i, j);
+					int rgb=image.getRGB(j, i);
 					int red=(int)Math.sin(cm.getRed(rgb));
 					if(red>255)
 						red=255;
 					if(red<0)
 						red=0;
-					result.setRGB(i, j, new Color(red,red,red).getRGB());
+					result.setRGB(j, i, new Color(red,red,red).getRGB());
 				}
 			}
 			return result;
-		}			
+		}	
+		
+	
 }
