091348c9a2e1af51305da2815b6909db4c9ea059
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-10-19 10:57:27 +0800

    Update Translation.java

--- Translation.java
+++ Translation.java
@@ -29,7 +29,7 @@ public class Translation{
 		JFrame childFrame=new JFrame();
 		childFrame.setTitle("图像平移");
 		childFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		childFrame.setBounds(400, 140, 280, 200);
+		childFrame.setBounds(400, 140, 280, 230);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		childFrame.setContentPane(contentPane);
@@ -56,10 +56,10 @@ public class Translation{
 		contentPane.add(distance);
 		distance.setColumns(10);
 		
-		JButton button = new JButton("\u786E\u8BA4\u5E73\u79FB");
-		button.setFont(new Font("黑体", Font.PLAIN, 12));
-		button.setBounds(132, 110, 80, 20);
-		button.addActionListener(new ActionListener() {
+		JButton nearest = new JButton("\u6700\u8FD1\u90BB\u63D2\u503C\u5E73\u79FB");//最近邻插值
+		nearest.setFont(new Font("黑体", Font.PLAIN, 12));
+		nearest.setBounds(132, 110, 120, 25);
+		nearest.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				String direct=direction.getText();
@@ -75,7 +75,28 @@ public class Translation{
 				childFrame.dispose();
 			}
 		});
-		contentPane.add(button);
+		contentPane.add(nearest);
+		
+		JButton biLinear = new JButton("\u53CC\u7EBF\u6027\u63D2\u503C\u5E73\u79FB");//双线性插值
+		biLinear.setFont(new Font("黑体", Font.PLAIN, 12));
+		biLinear.setBounds(132, 138, 120, 25);
+		biLinear.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String direct=direction.getText();
+				int dist=Integer.parseInt(distance.getText());
+				UI.temp=BitMap.translation(UI.image,direct,dist);
+				UI.picture2.setIcon(new ImageIcon(UI.temp));
+				UI.picture2.setText(null);
+				int hist[]=new int[256];
+				hist=BitMap.getHist(UI.temp);
+				int sum=UI.temp.getWidth()*UI.temp.getHeight();
+				BitMap.histLabel(sum, hist);
+				UI.histogram2.setIcon( new ImageIcon(BitMap.drawHist(hist,sum)));
+				childFrame.dispose();
+			}
+		});
+		contentPane.add(biLinear);
 	}
 
 }
